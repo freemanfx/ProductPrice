a19f1d43f3eb7e3ac427f7aad76c8c087ba6aa59
Silviu <freemanfx@gmail.com>
2014-02-16 00:00:28 +0200

    key for my mac :)

--- ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/MapDisplay.java
+++ ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/MapDisplay.java
@@ -1,7 +1,23 @@
 package ro.freemanfx.productprice.fragment;
 
 
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
 public class MapDisplay extends MapFragment {
+
+    private GoogleMap map;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map = getMap();
+    }
 }
