96c0b8b5b548b9386997e6b3279117929bc654db
Silviu Catarau <freemanfx@gmail.com>
2014-07-09 17:25:34 +0300

    fix orientation issues, upgraded versions, basic form validation

--- ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/AddProductFragment.java
+++ ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/AddProductFragment.java
@@ -6,15 +6,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+import ro.freemanfx.productprice.BeanProvider;
 import ro.freemanfx.productprice.Constants;
 import ro.freemanfx.productprice.R;
+import ro.freemanfx.productprice.domain.Place;
+import ro.freemanfx.productprice.domain.Product;
+import ro.freemanfx.productprice.infrastructure.LocationHelper;
 
 public class AddProductFragment extends Fragment implements Constants {
+    @InjectView(R.id.name)
+    TextView name;
+
     @InjectView(R.id.barcode)
     TextView barcode;
+
+    @InjectView(R.id.price)
+    TextView price;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +37,44 @@ public class AddProductFragment extends Fragment implements Constants {
         String barcodeString = getActivity().getIntent().getStringExtra(BARCODE);
         barcode.setText(barcodeString);
         return view;
+    }
+
+    @OnClick(R.id.add)
+    public void onAdd() {
+        if (!validInput()) {
+            return;
+        }
+
+        Place place = new Place("Home", LocationHelper.parseLocationString("10.22|22.22"));
+        BeanProvider.productService().addProduct(new Product(name.getText().toString(), barcode.getText().toString()), place, Double.parseDouble(price.getText().toString()));
+        getActivity().finish();
+    }
+
+    private boolean validInput() {
+        String nameString = name.getText().toString();
+        String priceString = price.getText().toString();
+        String barcodeString = barcode.getText().toString();
+
+        if (invalidString(nameString)) {
+            Toast.makeText(getActivity(), "Name is empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (invalidString(priceString)) {
+            Toast.makeText(getActivity(), "Price is empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (invalidString(barcodeString)) {
+            Toast.makeText(getActivity(), "Barcode is empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean invalidString(String nameString) {
+        return nameString == null || nameString.isEmpty();
     }
 
 }
