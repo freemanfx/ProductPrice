9740e3cb080843c531e64475dbf670e5f2b1c840
Silviu Catarau <freemanfx@gmail.com>
2014-07-10 10:38:25 +0300

    list of places

--- ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/FindProductFragment.java
+++ ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/FindProductFragment.java
@@ -7,10 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import ro.freemanfx.productprice.R;
+import ro.freemanfx.productprice.domain.Product;
 import ro.freemanfx.productprice.domain.ProductPrice;
 
+import static ro.freemanfx.productprice.BeanProvider.productPriceRepository;
+import static ro.freemanfx.productprice.BeanProvider.productRepository;
 import static ro.freemanfx.productprice.Constants.BARCODE;
 
 public class FindProductFragment extends ListFragment {
@@ -19,21 +26,46 @@ public class FindProductFragment extends ListFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         barcode = getActivity().getIntent().getStringExtra(BARCODE);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ListView view = (ListView) super.onCreateView(inflater, container, savedInstanceState);
-        view.setAdapter(new ProductsAdapter(barcode));
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        setListAdapter(new ProductsAdapter(barcode));
         return view;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(getActivity(), "Click on " + position, Toast.LENGTH_SHORT).show();
     }
 
     private class ProductsAdapter extends ArrayAdapter<ProductPrice> {
         public ProductsAdapter(String barcode) {
-            super(getActivity(), R.layout.found_products_fragment);
+            super(getActivity(), R.layout.product_price_list_item);
+            addProducts(barcode);
         }
 
-    }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.product_price_list_item, parent, false);
+            }
+            ProductPrice item = getItem(position);
+            TextView name = (TextView) view.findViewById(R.id.name);
+            name.setText(item.getPrice().toString());
+            return view;
+        }
 
+        private void addProducts(String barcode) {
+            //TODO: make this run on background thread ( rxJava ? )
+            Product product = productRepository().findByBarcode(barcode);
+            List<ProductPrice> byProduct = productPriceRepository().findByProduct(product);
+            addAll(byProduct);
+            notifyDataSetChanged();
+        }
+    }
 }
