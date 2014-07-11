302579ee3ccd372d3cd41e33d5d545e2553bf371
Silviu Catarau <freemanfx@gmail.com>
2014-07-11 12:00:11 +0300

    display markers on map

--- ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/MapDisplay.java
+++ ProductPrice/src/main/java/ro/freemanfx/productprice/fragment/MapDisplay.java
@@ -4,8 +4,15 @@ package ro.freemanfx.productprice.fragment;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import ro.freemanfx.productprice.Constants;
+import ro.freemanfx.productprice.domain.ProductPrice;
+
+import static ro.freemanfx.productprice.BeanProvider.productPriceRepository;
 
 public class MapDisplay extends SupportMapFragment {
 
@@ -20,5 +27,16 @@ public class MapDisplay extends SupportMapFragment {
     public void onResume() {
         super.onResume();
         map = getMap();
+
+        String barcode = getActivity().getIntent().getStringExtra(Constants.BARCODE);
+        List<ProductPrice> prices = productPriceRepository().findByProductBarcode(barcode);
+        for (ProductPrice productPrice : prices) {
+            map.addMarker(
+                    new MarkerOptions()
+                            .position(productPrice.getPlace().getLocation())
+                            .title(productPrice.getPrice().toString())
+                            .snippet(productPrice.getPlace().getName())
+            );
+        }
     }
 }
